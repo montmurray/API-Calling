fb35686fb13a1c67bd8eeff0deb571c103d473cd
montmurray <vinylscratch1103@gmail.com>
2025-03-05 09:30:04 -0600

    Initial Commit

---
+++ API Calling/API_CallingApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_CallingApp.swift
+//  API Calling
+//
+//  Created by Tessa Murray on 3/5/25.
+//
+
+import SwiftUI
+
+@main
+struct API_CallingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
