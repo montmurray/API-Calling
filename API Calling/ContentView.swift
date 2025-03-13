6e5b6e3cf222364c2438eafa5746db76c1b55557
montmurray <vinylscratch1103@gmail.com>
2025-03-13 09:47:24 -0500

    set up initial view, need to add facts.

--- API Calling/ContentView.swift
+++ API Calling/ContentView.swift
@@ -7,15 +7,51 @@
 
 import SwiftUI
 
+struct Planet: Identifiable, Codable {
+    var id: String
+    var englishName: String
+    var isPlanet: Bool
+}
+
+struct SolarSystemResponse: Codable {
+    let bodies: [Planet]
+}
+
 struct ContentView: View {
+    @State private var planets: [Planet] = []
+    @State private var showingAlert = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List(planets) { planet in
+                NavigationLink(destination: Text(planet.englishName)) {
+                    Text(planet.englishName)
+                }
+            }
+            .navigationTitle("Planets")
+            .task { await getPlanets() }
+            .alert(isPresented: $showingAlert) {
+                Alert(title: Text("Loading Error"),
+                      message: Text("An error has occured."),
+                      dismissButton: .default(Text("OK")))
+            }
         }
-        .padding()
+    }
+    
+    func getPlanets() async {
+        let query = "https://api.le-systeme-solaire.net/rest/bodies/"
+        guard let url = URL(string: query) else { return }
+        if let (data, _) = try? await URLSession.shared.data(from: url) {
+            if let decodedResponse = try? JSONDecoder().decode(SolarSystemResponse.self, from: data) {
+                DispatchQueue.main.async {
+                    planets = decodedResponse.bodies.filter { $0.isPlanet
+                    }
+                }
+                return
+            }
+        }
+        DispatchQueue.main.async {
+            showingAlert = true
+        }
     }
 }
 
