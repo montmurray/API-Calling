8de28cb74260c8ade485da07997645806c91febe
montmurray <vinylscratch1103@gmail.com>
2025-03-13 10:01:57 -0500

    Added fact view and facts.

---
+++ API Calling/FactsView.swift
@@ -0,0 +1,18 @@
+//
+//  FactsView.swift
+//  API Calling
+//
+//  Created by Tessa Murray on 3/13/25.
+//
+
+import SwiftUI
+
+struct FactsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FactsView()
+}
